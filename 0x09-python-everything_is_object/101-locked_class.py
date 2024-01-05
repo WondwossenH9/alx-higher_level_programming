a166e56e2f5ef5f72afb77450b0f7f31a440c989
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-01-05 10:19:35 -0800

    Write a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name.

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Prevents the user from dynamically creating new instance attributes, except for the new instance attribute called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
