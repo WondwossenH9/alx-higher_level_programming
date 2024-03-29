654d310832a3eed0b06874a8d05a2e156e47884f
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-03-29 06:22:56 -0700

    fix script import

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-The script takes URL as input, sends a request to the URL, and display
-the value of the variable X-Request-Id in the response header.
+The script takes a URL as input, sends a request to the URL and displays value of 
+variable X-Request-Id in the response header.
 """
 
 if __name__ == "__main__":
-    from requests import get
-    from sys import argv
+    import requests
+    import sys
 
     url = sys.argv[1]
     response = requests.get(url)
