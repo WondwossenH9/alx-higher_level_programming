02d45a8cd179f905dd71c4d88ad4fa3de2a321a3
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-03-29 05:48:52 -0700

    fix script

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
-# Creates a request object and opens the URL by a with statement
-# to automatically close the connection.
-# Gets the x-Request-Id variable value from response header
-# Checks if the script runs with URL argument and gets the URL
-# from the command line argument.
-def get_x_request_id(url):
-
-    req = urllib.request.Request(url)
-
-    with urllib.request.urlopen(req) as response:
-        x_request_id = response.getheader('X-Request-Id')
-        print(x_request_id)
+"""
+Displays the value of the X-Request-Id variable
+that is found in the header of the response.
+"""
 
 if __name__ == "__main__":
+    import sys
+    import urllib.request
+
     if len(sys.argv) != 2:
         print("Usage: python script_name.py <URL>")
         sys.exit(1)
 
-    url = sys.argv[1]
-    get_x_request_id(url)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        print(x_request_id)
